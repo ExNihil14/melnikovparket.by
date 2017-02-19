cc545bd659820e502c0efea61347416d1a6edb1a
ExNihil14 <ExNihil88@gmail.com>
2017-02-19 17:47:45 +0300

    Init

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+ 	$('.Mycarousel').slick({
+
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='images/back.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='images/right.png'>"
+
+	});
+
+	$('.Mycarousel2').slick({
+
+		prevArrow:"<img class='a-left control-c prev slick-prev' src='images/back.png'>",
+	    nextArrow:"<img class='a-right control-c next slick-next' src='images/right.png'>",
+
+	    slidesToShow: 5,
+		slidesToScroll: 1
+
+	});
+
+});
+
+
